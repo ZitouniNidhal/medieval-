e72b475c9963e7c9e9b4ff05ad8c2b2eeb2ab18a
ZITOUNI NIDHAL <nidhalzitouni111@gmail.com>
2025-02-05 23:10:23 +0100

    Update project configuration and refactor NewScript for improved structure and functionality

--- Scripts/NewScript.cs
+++ Scripts/NewScript.cs
@@ -1,22 +1,24 @@
 using Godot;
 using System;
 
-public partial class NewScript : Node
+namespace YourNamespaceName
 {
-    private Vector2 _velocity = new Vector2(100, 0); // Vitesse de déplacement
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public partial class NewScript : Node
     {
-        GD.Print("Node is ready!");
-    }
+        private Vector2 _velocity = new Vector2(100, 0); // Vitesse de déplacement
+        public Vector2 Position { get; set; } = new Vector2(); // Position du nœud
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        // Déplacer le nœud chaque frame
-        Position += _velocity * (float)delta;
-        var newPosition = Position + _velocity * (float)delta;
-        SetPosition(newPosition);
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+            GD.Print("Node is ready!");
+        }
+
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(double delta)
+        {
+            // Déplacer le nœud chaque frame
+            Position += _velocity * (float)delta;
+        }
     }
 }
