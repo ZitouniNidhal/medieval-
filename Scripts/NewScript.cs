fb0bc3b65c30197c2b91eb38429667267b6676a9
ZITOUNI NIDHAL <nidhalzitouni111@gmail.com>
2025-02-05 22:00:59 +0100

    Implement movement logic and initialization in NewScript

--- Scripts/NewScript.cs
+++ Scripts/NewScript.cs
@@ -3,13 +3,20 @@ using System;
 
 public partial class NewScript : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    private Vector2 _velocity = new Vector2(100, 0); // Vitesse de déplacement
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GD.Print("Node is ready!");
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        // Déplacer le nœud chaque frame
+        Position += _velocity * (float)delta;
+        var newPosition = Position + _velocity * (float)delta;
+        SetPosition(newPosition);
+    }
 }
